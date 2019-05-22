2ba387299e1ea56c027b9cb874a7e2c798f856c9
challenger1304 <v.neubauer@darlor.de>
2019-05-22 22:41:20 +0200

    now runnable on java-11 and up

---
+++ src/main/java/de/darlor/dacardconv/Main.java
@@ -0,0 +1,13 @@
+package de.darlor.dacardconv;
+
+/**
+ *
+ * @author Vincent Neubauer (v.neubauer@darlor.de)
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		DaCardConv.main(args);
+	}
+	
+}
