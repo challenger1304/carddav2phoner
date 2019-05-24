7b4ef32314735e578de5aed605bcc2f5c24083ed
challenger1304 <v.neubauer@darlor.de>
2019-05-24 11:54:37 +0200

    fixed for java 11

---
+++ src/main/java/de/darlor/dacardconv/Main.java
@@ -0,0 +1,13 @@
+package de.darlor.dacardconv;
+
+/**
+ *
+ * @author Vincent 'challenger1304' Neubauer (v.neubauer@darlor.de)
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		DaCardConv.main(args);
+	}
+	
+}
