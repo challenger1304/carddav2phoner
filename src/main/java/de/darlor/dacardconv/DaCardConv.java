9fc20a359994ec1de7a46fb67948905b6d2b8fba
challenger1304 <v.neubauer@darlor.de>
2019-05-29 12:28:17 +0200

    adding support for a settings-windows

--- src/main/java/de/darlor/dacardconv/DaCardConv.java
+++ src/main/java/de/darlor/dacardconv/DaCardConv.java
@@ -3,16 +3,22 @@ package de.darlor.dacardconv;
 import de.darlor.dacardconv.settings.BaseSettings;
 import de.darlor.dacardconv.panes.ConnectionPane;
 import de.darlor.dacardconv.panes.ImportExportPane;
+import de.darlor.dacardconv.panes.SettingsPane;
 import de.darlor.dacardconv.panes.VCardsPane;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -26,19 +32,41 @@ public class DaCardConv extends Application {
 	private VCardsPane vcardsPane;
 	private GridPane settingsPane;
 	private ConnectionPane connPane;
+	private VBox asidePane;
 
 	@Override
 	public void start(Stage primaryStage) {
 		settingsPane = new ImportExportPane(this).getPane();
 		vcardsPane = new VCardsPane(this);
 		connPane = new ConnectionPane(this);
+		asidePane = new VBox();
+		asidePane.setSpacing(4);
+		Button btSettings = new Button("Settings");
+		btSettings.setOnAction((event) -> {
+			Scene settingsScene = new Scene(SettingsPane.getPane(), 480, 360);
+			Stage settingsStage = new Stage();
+			settingsStage.setTitle(BaseSettings.getAppName() + " | Settings");
+			settingsStage.setScene(settingsScene);
+			settingsStage.getIcons().add(new Image("de/darlor/dacardconv/assets/logo.png"));
+			settingsStage.initOwner(primaryStage);
+			settingsStage.initModality(Modality.APPLICATION_MODAL);
+			settingsStage.showAndWait();
+//			settingsStage.setScene(value);
+		});
+		Region spacer01 = new Region();
+		spacer01.setPrefHeight(40);
+		VBox.setVgrow(spacer01, Priority.ALWAYS);
+		asidePane.getChildren().addAll(connPane.getPane(), spacer01); //TODO add btSettings
+		asidePane.widthProperty().addListener((ov, oldWidth, newWidth) -> {
+			btSettings.setMinWidth((double) newWidth);
+		});
 
 		BorderPane basicPane = new BorderPane();
 		basicPane.setPadding(new Insets(8));
 		BorderPane.setMargin(settingsPane, new Insets(4, 0, 0, 0));
-		BorderPane.setMargin(connPane.getPane(), new Insets(0, 4, 0, 0));
+		BorderPane.setMargin(asidePane, new Insets(0, 4, 0, 0));
 		basicPane.setBottom(settingsPane);
-		basicPane.setLeft(connPane.getPane());
+		basicPane.setLeft(asidePane);
 		basicPane.setCenter(vcardsPane.getPane());
 
 		Scene scene = new Scene(basicPane, 700, 500);
@@ -54,12 +82,13 @@ public class DaCardConv extends Application {
 	}
 
 	/**
-	 * Creates an Dialog object with predefined settings.
-	 * This dialog is resizable.
+	 * Creates an Dialog object with predefined settings. This dialog is
+	 * resizable.
 	 *
 	 * @param dialogName name of the dialog. will be of this scheme:
 	 * <pre>applicationName | dialogName</pre>
-	 * @return  a new Dialog object
+	 *
+	 * @return a new Dialog object
 	 */
 	public static Dialog getDialog(String dialogName) {
 		return getDialog(dialogName, true);
@@ -72,7 +101,7 @@ public class DaCardConv extends Application {
 	 * <pre>applicationName | dialogName</pre>
 	 *
 	 * @param resizable can this dialog shrink and grow in its size or not?
-	 * @return  a new Dialog object
+	 * @return a new Dialog object
 	 */
 	public static Dialog getDialog(String dialogName, boolean resizable) {
 		Dialog dialog = new Dialog();
---
+++ src/main/java/de/darlor/dacardconv/panes/SettingsPane.java
@@ -0,0 +1,20 @@
+package de.darlor.dacardconv.panes;
+
+import javafx.scene.layout.GridPane;
+
+/**
+ *
+ * @author Vincent Neubauer (v.neubauer@asysgmbh.de)
+ */
+public class SettingsPane {
+
+	private static final GridPane PANE = new GridPane();
+
+	public static GridPane getPane() {
+
+		return PANE;
+	}
+
+
+
+}
